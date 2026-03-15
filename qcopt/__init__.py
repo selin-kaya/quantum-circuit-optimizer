be47313f59107cd6bd1a5c951b8f5e419019d334
selin-kaya <135339669+selin-kaya@users.noreply.github.com>
2026-03-15 22:28:32 +0000

    Exported circuit optimization and metrics

---
+++ qcopt/__init__.py
@@ -0,0 +1,4 @@
+from .optimizer import optimize_circuit, OptimizationResult
+from .metrics import compute_metrics, CircuitMetrics
+
+__all__ = ["optimize circuit", "OptimizationResult", "compute_metrics", "CircuitMetrics"]
